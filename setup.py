d1b3d7c730831ec7641fc7a309857459b1006ebb
HipyCas <hipycas@gmail.com>
2020-07-24 13:00:26 -0400

    Updated name to Utilities-Lib

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Utilities",
+    name="Utilities-Lib",
     version="0.0.1",
     author="HipyCas",
     author_email="hipycas+python@gmail.com",
